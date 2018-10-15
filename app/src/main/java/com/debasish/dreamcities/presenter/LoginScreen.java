3a030e3c5e591f032d936a0c5cea95f289d55037
Debasish <debasishkumardas5@gmail.com>
2018-10-15 16:03:02 +0530

    Login Validation added.

--- app/src/main/java/com/debasish/dreamcities/presenter/LoginScreen.java
+++ app/src/main/java/com/debasish/dreamcities/presenter/LoginScreen.java
@@ -3,10 +3,12 @@ package com.debasish.dreamcities.presenter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.debasish.dreamcities.R;
+import com.debasish.dreamcities.view.ILoginView;
 
-public class LoginScreen extends AppCompatActivity {
+public class LoginScreen extends AppCompatActivity implements ILoginView {
 
 
     EditText etUserId;
@@ -16,5 +18,15 @@ public class LoginScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+    }
+
+    @Override
+    public void onLoginResult(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onLoginValidation(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
